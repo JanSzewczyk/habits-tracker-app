097e88d6d89d5497d4a69f61fb2572cd94271414
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-03 20:55:30 +0200

    feat: add tabs layout

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { Link } from "expo-router";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Login</Text>
+      <Link href="/login">sdfsaf</Link>
+    </View>
+  );
+}
