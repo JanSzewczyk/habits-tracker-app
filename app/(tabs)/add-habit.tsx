e264bc047f613bd37587ee290e7d13fc78cb6572
JanSzewczyk <jan.szewczyk1997@gmail.com>
2025-08-06 22:14:51 +0200

    feat: add habits form and add react-hook-form lib

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,10 +1,129 @@
-import { View, Text } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+import { Button, SegmentedButtons, Text, TextInput } from "react-native-paper";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FREQUENCIES, habitSchema, HabitSchemaType } from "~/schemas/habit";
+import { useAuth } from "~/contexts/auth-context";
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "~/lib/appwrite";
+import { ID } from "react-native-appwrite";
+import { useRouter } from "expo-router";
 
 export default function AddHabitScreen() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: zodResolver(habitSchema)
+  });
+
+  async function onSubmit(data: HabitSchemaType) {
+    if (!user) {
+      return;
+    }
+
+    try {
+      await databases.createDocument(DATABASE_ID, HABITS_COLLECTION_ID, ID.unique(), {
+        userId: user.$id,
+        title: data.title,
+        description: data.description,
+        frequency: data.frequency,
+        streakCount: 0,
+        lastCompleted: new Date().toISOString(),
+        createdAt: new Date().toISOString()
+      });
+
+      router.back();
+    } catch (e) {
+      if (e instanceof Error) {
+        console.error("Error creating habit:", e.message);
+      }
+      console.log("There was an error creating the habit:", e);
+    }
+  }
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={styles.formFieldContainer}>
+        <Controller
+          control={control}
+          name="title"
+          render={({ field: { onChange, value } }) => (
+            <TextInput label="Title" mode="outlined" value={value} onChangeText={onChange} error={!!errors.title} />
+          )}
+        />
+        {errors.title ? (
+          <Text variant="bodySmall" style={styles.error}>
+            {errors.title.message}
+          </Text>
+        ) : null}
+      </View>
+
+      <View style={styles.formFieldContainer}>
+        <Controller
+          control={control}
+          name="description"
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              label="Description"
+              mode="outlined"
+              value={value}
+              onChangeText={onChange}
+              error={!!errors.description}
+            />
+          )}
+        />
+        {errors.description ? (
+          <Text variant="bodySmall" style={styles.error}>
+            {errors.description.message}
+          </Text>
+        ) : null}
+      </View>
+
+      <View style={styles.formFieldContainer}>
+        <Controller
+          control={control}
+          name="frequency"
+          render={({ field: { onChange, value } }) => (
+            <SegmentedButtons
+              buttons={FREQUENCIES.map((frequency) => ({
+                value: frequency,
+                label: frequency.charAt(0).toUpperCase() + frequency.slice(1)
+              }))}
+              value={value}
+              onValueChange={onChange}
+              multiSelect={false}
+            />
+          )}
+        />
+        {errors.frequency ? (
+          <Text variant="bodySmall" style={styles.error}>
+            {errors.frequency.message}
+          </Text>
+        ) : null}
+      </View>
+
+      <Button mode="contained" onPress={handleSubmit(onSubmit)}>
+        Add Habit
+      </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+    justifyContent: "center"
+  },
+
+  formFieldContainer: {
+    marginBottom: 16
+  },
+
+  error: { color: "red", marginTop: 8 }
+});
